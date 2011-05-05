25f67bf4da7917a4b9aeb099475c95f1b0149f28
Laurent Laborde <kerdezixe@gmail.com>
2011-05-05 13:56:49 +0200

    added dlog function

--- bootstrap/bootstrap.c
+++ bootstrap/bootstrap.c
@@ -6,9 +6,26 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdarg.h>	//for dlog
+#include <unistd.h>	//for getopt
 
+int DEBUG = 1;
 
+void dlog(const char* text, ...) {
+	if(DEBUG == 1 && text) { 
+		va_list argp;
+		va_start(argp, text);
+		fprintf(stderr,text, argp); 
+		va_end(argp);
+	}
+}
 
-int main(void) {
+int main(int argc, char *argv[]) {
+	printf("-----------------------------\n");
+	printf("Parenthese's Scheme bootstrap\n");
+	printf("-----------------------------\n\n");
+
+	dlog("/!\\ Running in debug mode !\n\n");
+
 	return 0;
 }
